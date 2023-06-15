ac76568f3d1af00b2677b946525b668a9b320401
Keijo Kapp <keijo.kapp@gmail.com>
2023-06-15 20:07:08 +0300

    Added ability to specify subprocess executable paths

--- bin/create-musescore-video.js
+++ bin/create-musescore-video.js
@@ -1,17 +1,71 @@
 #!/usr/bin/env node
 
+import module from 'module';
+import util from 'util';
 import chalk from 'chalk';
 import { scoreMedia } from '../lib/utils.js';
 import createVideo from '../lib/create-video.js';
 
-if (process.argv.length !== 4) {
-	console.error('Usage: create-musescore-video input.mscz output.mp4');
-	process.exit(1);
+function getArgs() {
+	try {
+		return util.parseArgs({
+			allowPositionals: true,
+			options: {
+				ffmpeg: {
+					type: 'string',
+					default: 'ffmpeg'
+				},
+				help: {
+					type: 'boolean',
+					short: 'h'
+				},
+				mscore: {
+					type: 'string',
+					default: 'mscore'
+				},
+				version: {
+					type: 'boolean',
+					short: 'v'
+				}
+			}
+		});
+	} catch (e) {
+		console.error(e.message);
+
+		process.exit(1);
+	}
 }
 
-const [, , musescoreFile, videoFile] = process.argv;
+function getVersion() {
+	const require = module.createRequire(import.meta.url);
+
+	return require('../package.json').version;
+}
+
+const args = getArgs();
+
+if (args.values.version) {
+	console.log(getVersion());
+
+	process.exit(0);
+}
+
+if (args.values.help || args.positionals.length !== 2) {
+	console.log(`Usage: create-musescore-video [--ffmpeg=ffmpeg] [--mscore=mscore] input.mscz output.mp4
+
+Options:
+ -h,--help       show help text
+ -v,--version    show version
+ --ffmpeg=FILE   path to ffmpeg executable
+ --mscore=FILE   path to MuseScore (mscore) executable
+`);
+
+	process.exit(args.values.help ? 0 : 1);
+}
+
+const [musescoreFile, videoFile] = args.positionals;
 
 console.log('Loading score media for %s', chalk.bold(musescoreFile));
-const mediaInfo = await scoreMedia(musescoreFile);
+const mediaInfo = await scoreMedia(musescoreFile, { mscore: args.values.mscore });
 
-await createVideo(mediaInfo, videoFile);
+await createVideo(mediaInfo, videoFile, { ffmpeg: args.values.ffmpeg });
