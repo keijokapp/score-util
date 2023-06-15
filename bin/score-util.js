1ea6dc3eee489d34745083a6d0a015ffdaa75351
Keijo Kapp <keijo.kapp@gmail.com>
2023-06-15 20:07:08 +0300

    Added ability to specify subprocess executable paths

--- bin/score-util.js
+++ bin/score-util.js
@@ -3,10 +3,74 @@
 import assert from 'assert';
 import { execFileSync } from 'child_process';
 import fs from 'fs/promises';
+import module from 'module';
+import util from 'util';
 import path from 'path';
 import chalk from 'chalk';
 import createVideo from '../lib/create-video.js';
 import { getAudioLength, scoreMedia } from '../lib/utils.js';
+
+function getArgs() {
+	try {
+		return util.parseArgs({
+			allowPositionals: true,
+			options: {
+				ffmpeg: {
+					type: 'string',
+					default: 'ffmpeg'
+				},
+				ffprobe: {
+					type: 'string',
+					default: 'ffprobe'
+				},
+				help: {
+					type: 'boolean',
+					short: 'h'
+				},
+				mscore: {
+					type: 'string',
+					default: 'mscore'
+				},
+				version: {
+					type: 'boolean',
+					short: 'v'
+				}
+			}
+		});
+	} catch (e) {
+		console.error(e.message);
+
+		process.exit(1);
+	}
+}
+
+function getVersion() {
+	const require = module.createRequire(import.meta.url);
+
+	return require('../package.json').version;
+}
+
+const args = getArgs();
+
+if (args.values.version) {
+	console.log(getVersion());
+
+	process.exit(0);
+}
+
+if (args.values.help || args.positionals.length !== 0) {
+	console.log(`Usage: score-util [--ffmpeg=ffmpeg] [--ffprobe=ffprobe] [--mscore=mscore]
+
+Options:
+ -h,--help       show help text
+ -v,--version    show version
+ --ffmpeg=FILE   path to ffmpeg executable
+ --ffprobe=FILE  path to ffprobe executable
+ --mscore=FILE   path to MuseScore (mscore) executable
+`);
+
+	process.exit(args.values.help ? 0 : 1);
+}
 
 const rootDirectory = path.resolve('.');
 const audioDirectory = path.join(rootDirectory, 'audio');
@@ -36,13 +100,13 @@ const lengths = await Promise.all(audioFiles.map(
 assert(lengths.every(length => length === lengths[0]));
 
 console.log('Loading score media for %s', chalk.bold(path.basename(musescoreFile)));
-const mediaInfo = await scoreMedia(musescoreFile);
+const mediaInfo = await scoreMedia(musescoreFile, { mscore: args.values.mscore });
 
 console.log('Creating PDF %s', chalk.bold(path.basename(pdfFile)));
 await fs.writeFile(pdfFile, Buffer.from(mediaInfo.pdf, 'base64'));
 
 console.log('Creating video %s', chalk.bold(path.basename(videoFile)));
-await createVideo(mediaInfo, videoFile);
+await createVideo(mediaInfo, videoFile, { ffmpeg: args.values.ffmpeg });
 
 await fs.mkdir(exportDirectory).catch(e => {
 	if (e.code !== 'EEXIST') {
@@ -55,7 +119,7 @@ for (const audioFile of audioFiles) {
 
 	console.log('Creating video %s', chalk.bold(path.basename(exportVideoFile)));
 
-	execFileSync('ffmpeg', [
+	execFileSync(args.values.ffmpeg, [
 		'-y',
 		'-i', videoFile,
 		'-i', path.join(audioDirectory, audioFile),
